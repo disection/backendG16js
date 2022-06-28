fa6c948bb132bb311b79557b7476091e2b102fdd
disection <diseccionmexico@gmail.com>
2022-06-28 14:51:58 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+/**
+ * hacer una funcion que reciba como parametro un nombre y que imprima , Hola {nombre}, como estan?
+ */
+
+function nombre(nombre){
+    return `Hola ${nombre}, como estan?`
+}
+console.log(nombre("lore"))
+
+/**
+ * 
+ */
+const nombres = [
+    "Paco",
+    "Lorena",
+    "Kike",
+    "Cris",
+    "Victor",
+    "Marco",
+    "Israel",
+    "Annie"
+]
+
